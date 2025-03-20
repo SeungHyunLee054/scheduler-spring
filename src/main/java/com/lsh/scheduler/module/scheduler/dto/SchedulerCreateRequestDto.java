be7c18ea48f14c3bf07ba76faef61514ad347156
SeungHyunLee054 <yellow13lsh@naver.com>
2025-03-20 13:34:25 +0900

    Refactor : 요청 dto에 검증 어노테이션 누락
    SchedulerCreateRequestDto에 memberId의 null이 아닌 값을 valid하는 어노테이션이 누락되어 추가

--- src/main/java/com/lsh/scheduler/module/scheduler/dto/SchedulerCreateRequestDto.java
+++ src/main/java/com/lsh/scheduler/module/scheduler/dto/SchedulerCreateRequestDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SchedulerCreateRequestDto {
+    @NotNull
     private Long memberId;
     @Size(max = 200)
     @NotNull
