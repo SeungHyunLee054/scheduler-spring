231868fa82a1cd56a8b9c275d1272a085bd74c56
SeungHyunLee054 <yellow13lsh@naver.com>
2025-03-21 11:40:48 +0900

    Refactor : 컬럼명을 snake_case로 변경
    컬럼명을 snake_case로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,8 +7,8 @@ create table member
     id         bigint       not null auto_increment primary key,
     name       varchar(50)  not null,
     email      varchar(320) not null,
-    createdAt  datetime     not null default current_timestamp,
-    modifiedAt datetime     not null default current_timestamp on update current_timestamp
+    created_at  datetime     not null default current_timestamp,
+    modified_at datetime     not null default current_timestamp on update current_timestamp
 );
 
 
@@ -17,8 +17,8 @@ create table scheduler
     id         bigint       not null auto_increment primary key,
     task       varchar(255) not null,
     password   varchar(72)  not null,
-    createdAt  datetime     not null default current_timestamp,
-    modifiedAt datetime     not null default current_timestamp on update current_timestamp,
+    created_at  datetime     not null default current_timestamp,
+    modified_at datetime     not null default current_timestamp on update current_timestamp,
     member_id  bigint       not null,
     foreign key (member_id) references member (id) on DELETE cascade
 );
