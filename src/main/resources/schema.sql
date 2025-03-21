e356e96c3f1455f15f9c839b46864d161adc0422
SeungHyunLee054 <yellow13lsh@naver.com>
2025-03-21 15:09:16 +0900

    Style : 줄 재정렬
    줄 재정렬

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,9 +4,9 @@ drop table if exists scheduler,member;
 
 create table member
 (
-    id         bigint       not null auto_increment primary key,
-    name       varchar(50)  not null,
-    email      varchar(320) not null,
+    id          bigint       not null auto_increment primary key,
+    name        varchar(50)  not null,
+    email       varchar(320) not null,
     created_at  datetime     not null default current_timestamp,
     modified_at datetime     not null default current_timestamp on update current_timestamp
 );
@@ -14,11 +14,11 @@ create table member
 
 create table scheduler
 (
-    id         bigint       not null auto_increment primary key,
-    task       varchar(255) not null,
-    password   varchar(72)  not null,
+    id          bigint       not null auto_increment primary key,
+    task        varchar(255) not null,
+    password    varchar(72)  not null,
     created_at  datetime     not null default current_timestamp,
     modified_at datetime     not null default current_timestamp on update current_timestamp,
-    member_id  bigint       not null,
+    member_id   bigint       not null,
     foreign key (member_id) references member (id) on DELETE cascade
 );
