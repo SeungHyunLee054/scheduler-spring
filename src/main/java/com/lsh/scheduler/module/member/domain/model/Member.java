fbcd5379452e1b002bb634d79d872d18e6744105
SeungHyunLee054 <yellow13lsh@naver.com>
2025-03-21 11:42:08 +0900

    Refactor : 잘못 선언된 어노테이션 제거
    잘못 선언된 어노테이션 제거

--- src/main/java/com/lsh/scheduler/module/member/domain/model/Member.java
+++ src/main/java/com/lsh/scheduler/module/member/domain/model/Member.java
@@ -1,7 +1,5 @@
 package com.lsh.scheduler.module.member.domain.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,15 +8,9 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class Member {
-    @NotNull
     private Long id;
-    @NotNull
     private String name;
-    @NotNull
-    @Email
     private String email;
-    @NotNull
     private LocalDateTime createdAt;
-    @NotNull
     private LocalDateTime modifiedAt;
 }
